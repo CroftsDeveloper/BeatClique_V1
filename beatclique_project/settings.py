664f5a4e196e6e7ecae16bf8f1b65f4d8f7ac15e
CroftsDeveloper <crofts.webdeveloper@gmail.com>
2024-02-05 20:58:35 +0000

    Added FAQ Section with Bootstrap Accordion
    - Added a Frequently Asked Questions (FAQ) section to the homepage.
    - Implemented Bootstrap  accordion component for collapsible dropdowns.
    - Included four FAQ items with clickable questions and answers.

--- beatclique_project/settings.py
+++ beatclique_project/settings.py
@@ -116,8 +116,8 @@ USE_TZ = True
 # Static files configuration
 
 STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 
