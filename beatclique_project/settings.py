9b3b1368cde2b42888d549f3f91b2f7dda17bf83
CroftsDeveloper <crofts.webdeveloper@gmail.com>
2024-02-22 22:29:21 +0000

    Updated EMAIL_PORT setting to attempt to fix Heroku deployment issue. Changed from EMAIL_PORT = os.getenv('EMAIL_PORT', cast=int) to EMAIL_PORT = int(os.getenv('EMAIL_PORT'))
    Removed .env

--- beatclique_project/settings.py
+++ beatclique_project/settings.py
@@ -84,7 +84,7 @@ STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY')
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.getenv('EMAIL_HOST')
-EMAIL_PORT = os.getenv('EMAIL_PORT', cast=int)
+EMAIL_PORT = int(os.getenv('EMAIL_PORT'))
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', cast=bool)
